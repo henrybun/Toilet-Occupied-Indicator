16a6977e1cbca92665fa63fe06aa72b37c53dad0
henrybun <henrybun1994@gmail.com>
2022-01-12 13:33:41 +0800

    For testing wifi connection

---
+++ WifiTest/WifiTest.ino
@@ -0,0 +1,87 @@
+#include <SPI.h>
+
+#include <WiFiNINA.h>
+#include <WiFiUdp.h>
+#include "arduino_secrets.h" 
+
+int status = WL_IDLE_STATUS;
+
+//WiFi
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;            // your network key index number (needed only for WEP)
+
+unsigned int localPort = 2390;      // local port to listen on
+
+char packetBuffer[256]; //buffer to hold incoming packet
+char  ReplyBuffer[] = "hi";       // a string to send back
+WiFiUDP Udp;
+
+void setup()
+{
+  Serial.begin(9600);
+  //pinMode(,INPUT)
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 5 seconds for connection:
+    delay(5000);
+  }
+  Serial.println("Connected to WiFi");
+  printWifiStatus();
+
+  Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  Udp.begin(localPort);
+}
+
+void loop()
+{
+  //int reading=anglogRead(,)
+    int packetSize = Udp.parsePacket();
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    Udp.println("I'm 192.168.0.101"); //Message send to server
+    Serial.println("I'm 192.168.0.101"); //Print message send to server
+    
+    int len = Udp.read(packetBuffer, 255);
+    if (len > 0) {
+      packetBuffer[len] = 0;
+    }
+    Serial.println("Received contents:");
+    Serial.println(packetBuffer);
+    Udp.endPacket();
+    delay(1000);
+}
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
